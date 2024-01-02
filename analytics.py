2ec89ef2f1bfc0e012eada02946930400241070f
Chukwuemeka <mekastans@gmail.com>
2024-01-02 22:27:09 +0100

    Updated Email Sender

--- analytics.py
+++ analytics.py
@@ -170,3 +170,70 @@
         
 # #         print(f"Orders so far today: {total_orders_count}")
 # #         print(f"Revenue so far today: {total_revenue}")
+
+
+
+from config import config
+from confluent_kafka import DeserializingConsumer
+from confluent_kafka.schema_registry.avro import AvroDeserializer
+from confluent_kafka.serialization import StringDeserializer
+from confluent_kafka.schema_registry import SchemaRegistryClient
+
+
+# Topic Name
+ORDER_CONFIRMED_KAFKA_TOPIC = "order_confirmed"
+
+
+##### Consumer Configurations #####
+schema_registry_client = SchemaRegistryClient(config["schema_registry"])
+avro_schema = schema_registry_client.get_latest_version(f"{ORDER_CONFIRMED_KAFKA_TOPIC}-value")
+value_schema = avro_schema.schema.schema_str
+avro_deserializer = AvroDeserializer(schema_registry_client, value_schema)
+
+
+kafka_config = config["kafka"]
+consumer_config = {
+    'key.deserializer': StringDeserializer('utf_8'),
+    'value.deserializer': avro_deserializer,
+    'group.id': "orders_consumer_group",
+    'auto.offset.reset': "latest"
+}
+consumer_config.update(kafka_config)
+
+# Instantiating the Consumer
+consumer = DeserializingConsumer(consumer_config)  
+
+# Subscribe to the topic 'order_details'
+consumer.subscribe([ORDER_CONFIRMED_KAFKA_TOPIC])  
+
+emails_sent_to = set()
+             
+try:
+    print("Listening for Messages..")
+    while True:
+        msg = consumer.poll(1.0)
+
+        if msg is None:
+            continue
+
+        # Data Extraction
+        consumed_message = msg.value()
+        email = consumed_message["EMAIL"] 
+        emails_sent_to.add(email)
+        
+        if len(emails_sent_to) > 1:
+            print(f"""
+                  {len(emails_sent_to)} email sent
+                  {email} was added to the list.
+                  """)
+        else:
+            print(f"""
+                  {len(emails_sent_to)} email sent
+                  {email} was added to the list.
+                  """)
+
+except KeyboardInterrupt:
+    print("User Interrupt!!")
+finally:
+    # Close down consumer to commit final offsets.
+    consumer.close()
--- send-email.py
+++ send-email.py
@@ -1,26 +1,64 @@
-# import json
-# from kafka import KafkaConsumer
+from config import config
+from confluent_kafka import DeserializingConsumer
+from confluent_kafka.schema_registry.avro import AvroDeserializer
+from confluent_kafka.serialization import StringDeserializer
+from confluent_kafka.schema_registry import SchemaRegistryClient
 
-# # Topic Name
-# ORDER_CONFIRMED_KAFKA_TOPIC = "order_confirmed"
 
-# # Creating Consumer
-# consumer = KafkaConsumer(
-#     ORDER_CONFIRMED_KAFKA_TOPIC,
-#     bootstrap_servers="localhost:29092"
-# )
+# Topic Name
+ORDER_CONFIRMED_KAFKA_TOPIC = "order_confirmed"
 
-# emails_sent_to = set()
-# print("Listening for Events...")
-# while True:
-#     for message in consumer:
-#         consumed_message = json.loads(message.value.decode())
-#         customer_email = consumed_message["customer_email"]
+
+##### Consumer Configurations #####
+schema_registry_client = SchemaRegistryClient(config["schema_registry"])
+avro_schema = schema_registry_client.get_latest_version(f"{ORDER_CONFIRMED_KAFKA_TOPIC}-value")
+value_schema = avro_schema.schema.schema_str
+avro_deserializer = AvroDeserializer(schema_registry_client, value_schema)
+
+
+kafka_config = config["kafka"]
+consumer_config = {
+    'key.deserializer': StringDeserializer('utf_8'),
+    'value.deserializer': avro_deserializer,
+    'group.id': "orders_consumer_group",
+    'auto.offset.reset': "latest"
+}
+consumer_config.update(kafka_config)
+
+# Instantiating the Consumer
+consumer = DeserializingConsumer(consumer_config)  
+
+# Subscribe to the topic 'order_details'
+consumer.subscribe([ORDER_CONFIRMED_KAFKA_TOPIC])  
+
+emails_sent_to = set()
+             
+try:
+    print("Listening for Transactions..")
+    while True:
+        msg = consumer.poll(1.0)
+
+        if msg is None:
+            continue
+
+        # Data Extraction
+        consumed_message = msg.value()
+        email = consumed_message["EMAIL"] 
+        emails_sent_to.add(email)
         
-#         print(f"Sending confirmation email to {customer_email}..")
-        
-#         emails_sent_to.add(customer_email)
-#         if len(emails_sent_to) > 1:
-#             print(f"{len(emails_sent_to)} emails sent ..")
-#         else:
-#             print(f"{len(emails_sent_to)} email sent ..")
+        if len(emails_sent_to) > 1:
+            print(f"""
+                  {len(emails_sent_to)} email sent
+                  {email} was added to the list.
+                  """)
+        else:
+            print(f"""
+                  {len(emails_sent_to)} email sent
+                  {email} was added to the list.
+                  """)
+
+except KeyboardInterrupt:
+    print("User Interrupt!!")
+finally:
+    # Close down consumer to commit final offsets.
+    consumer.close()
